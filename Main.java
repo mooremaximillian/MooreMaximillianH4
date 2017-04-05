349a4c2d05ee93d59b6f32aaf66eac3f81996655
Maximillian Moore <mooremaximillian@gmail.com>
2017-04-05 17:35:12 -0400

    Creates enqueue and dequeue methods but still coming up with errors in main method

--- Main.java
+++ Main.java
@@ -7,15 +7,29 @@ public class Main
    Node book2 = new Node("Book 2");
    Node book3 = new Node("Book 3");
    Node book4 = new Node("Book 4");
+    Node book5 = new Node("Book 5");
+   Node book6 = new Node("Book 6");
+   Node book7 = new Node("Book 7");
+   Node book8 = new Node("Book 8"); 
    
    Stack collection = new Stack();
+   Queue queue = new Queue();
    
-   collection.setTop(book1);
-   collection.setTop(book2);
-   collection.setTop(book3);
-   collection.setTop(book4);
+   queue.setFront(book5);
+   queue.setBack(book6);
+   // queue.enqueue(book6);
+    queue.enqueue(book7);
+    queue.enqueue(book8);
+    
+   
+   collection.push(book2);
+   collection.push(book3);
+   collection.push(book4);
    
    collection.print();
+   
+ 
+   queue.print();
    
    
    
--- Queue.java
+++ Queue.java
@@ -1,60 +1,70 @@
-/**
- * This Class defines the info in each Node as well as the operations to the list and Node
- **/
-
 public class Queue
 {
-  // initializes the names that will be included in lists
-  String name;
- // intializes the next node
-  Queue next;
- 
- 
- 
- //Consturctor to input a new name 
- public Queue(String name, Queue n)
+  private Node front = null;
+  
+  private Node back = null;
+  
+  public void setFront(Node newQueue)
+  {
+    this.front = newQueue;
+  }
+  public void setBack(Node newQueue)
+  {
+    this.back = newQueue;
+  }
+  
+  public void enqueue(Node newQueue)
+  {
+    if(this.isEmpty())
+    {
+      this.back = newQueue;
+      this.front = newQueue;
+    }
+    else
+    {
+      this.front.setNext(newQueue);
+      this.front = newQueue;
+      
+    }
+  }
+  
+  public Node dequque()
+  {
+    if(this.isEmpty())
+      return null;
+    else
+    {
+      Node tmp = this.front;
+      this.front = back.getNext();
+      if(this.front == null)
+      
+        this.back = null;
+        return tmp;
+    }
+  }
+        
+        
+  
+   public void print()
  {
-   next = n;
-   this.name = name;
-   
- }
- /**
-  * Constructor to just input the string
-  */ 
- Queue(String n)
- {
-   this(n, null);
- }
- /**
-  * Retrieves the Node that the current node points to
-  */ 
- public Queue getNext()
- {
-   return next;
- }
- 
- 
- public String getName()
- {
-   return name;
- }
- 
- 
- public void setNext(Queue queue)
- {
-   this.next = queue;
- }
- 
-
- public void setName(String name)
- {
-   this.name = name;
-   
- }
- 
- public String toString() {
-  return " Name : "+this.name;
+   if(this.isEmpty())
+   {
+     System.out.println("The list is empty mane");
+   }
+   else
+   {
+     Node tmp = this.front;
+     while(tmp != null)
+     {
+       System.out.println(tmp);
+       tmp = tmp.getNext();
+     }
+     System.out.println("");
+   }
  }
   
-                       
+  public boolean isEmpty()
+ {
+     return this.front == null;
+   }
 }
